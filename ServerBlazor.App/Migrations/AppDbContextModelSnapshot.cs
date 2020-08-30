2638671296e27181a2d35a2242da307bf906574e
Hydrapepe <66299521+Hydrapepe@users.noreply.github.com>
2020-08-30 13:07:06 +0300

    reliz

--- ServerBlazor.App/Migrations/AppDbContextModelSnapshot.cs
+++ ServerBlazor.App/Migrations/AppDbContextModelSnapshot.cs
@@ -2,13 +2,12 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ServerBlazor.Data;
 
 namespace ServerBlazor.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    class AppDbContextModelSnapshot : ModelSnapshot
     {
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
