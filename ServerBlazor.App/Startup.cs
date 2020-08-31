3bd922044403868fb4b0931521ecbbbe1648c327
Hydrapepe <66299521+Hydrapepe@users.noreply.github.com>
2020-08-31 13:35:23 +0300

    test testa

--- ServerBlazor.App/Startup.cs
+++ ServerBlazor.App/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ServerBlazor.Authorization;
@@ -20,13 +19,6 @@ namespace ServerBlazor
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        private IConfiguration Configuration { get; }
-
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(options =>
